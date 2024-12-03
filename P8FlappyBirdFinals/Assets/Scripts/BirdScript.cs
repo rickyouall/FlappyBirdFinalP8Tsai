4226d3190837fc815bcdfca88dfae4005320ecd1
rickyouall <740502@student.dusd.net>
2024-12-03 15:27:33 -0800

    creating the background and main components in game
    today in class, i had created a background, foreground, and the bird for the project.

---
+++ P8FlappyBirdFinals/Assets/Scripts/BirdScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    private bool IsDead = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsDead == false)
+        {
+            if (Input.GetKeyDown) {
+        }
+    }
+}
